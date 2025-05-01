570165ddd2b7544714144f286adb3a78b53c48b4
R3TR0R0C4 <nicoroca04@gmail.com>
2025-05-01 17:51:52 +0200

    Update ciscoLogger.sql

--- ciscoLogger.sql
+++ ciscoLogger.sql
@@ -13,3 +13,26 @@ CREATE TABLE switch_interface_log_180_238 (
     speed VARCHAR(20),                  -- Interface speed (e.g., '1000 Mbps', '100 Mbps')
     UNIQUE KEY (switch_ip, interface_name, log_time) -- Added to prevent duplicate entries for the same interface and timestamp.
 );
+
+
+
+CREATE TABLE interface_stats (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    interface_name VARCHAR(255) NOT NULL,
+    last_input DATETIME,
+    last_output DATETIME,
+    log_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    description VARCHAR(255),
+    duplex_status VARCHAR(50),
+    speed VARCHAR(50),
+    vlan VARCHAR(50),
+    mac VARCHAR(50),
+    status VARCHAR(50),
+    switchport BOOLEAN,
+    admin_status VARCHAR(50),
+    port_mode VARCHAR(50),
+    native_vlan VARCHAR(50),
+    INDEX (interface_name, log_time),
+    INDEX (status, log_time),
+    INDEX (vlan)
+);
