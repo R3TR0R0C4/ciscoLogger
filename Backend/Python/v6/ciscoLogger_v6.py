5528196d2f3545037e12625f51243e19abb0df6a
R3TR0R0C4 <nicoroca04@gmail.com>
2025-05-14 09:42:03 +0000

    Implemented REGEX filtering of Interface info

--- Backend/Python/v6/ciscoLogger_v6.py
+++ Backend/Python/v6/ciscoLogger_v6.py
@@ -2,6 +2,9 @@ import json
 import netmiko
 from netmiko import ConnectHandler 
 from os import path
+import re
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 
 def read_json_configs(filepath, read_type):
@@ -23,12 +26,20 @@ def read_json_configs(filepath, read_type):
         print(f"Error: format JSON no valid {filepath}")
         return None
 
-def get_interface_info(device_details,interface_type, interface_number):
+def get_interface_info(device_details,interface_type,interface_number):
+    """
+    Inputs:
+        <device_details> Dictionary with the connection details 
+        <interface_type> Type of interface (either gigabitethernet or fasterthernet)
+        <interface_number> (number range ex 1-48 1-2)
+    Return:
+        List with the output of the command of each interface as one item
+    """
     if not device_details:
         return []
     
-
-    conn_info = {
+    #Dict to make the connection info
+    conn_info = { 
         "host": device_details['host'],
         "username": device_details['username'],
         "password": device_details['password'],
@@ -36,20 +47,21 @@ def get_interface_info(device_details,interface_type, interface_number):
         "port": 23,
     }
 
-
+    #Try to connect and retrieve each of the interfaces current config
     try:
         net_connect = ConnectHandler(**conn_info)
-
         print(f"Retrieving interface information from {device_details['host']} for {interface_type}{interface_number}")
 
-
+        #Transform the range (ex 1-48) to something usable ( range(1-49) )
         start, end = map(int, interface_number.split("-"))
         interface_range = range(start, end+1)
 
+        #Command list
         interfaces_to_query=[]
         for interface in interface_range:
             interfaces_to_query.append(f"{interface_type}{interface}")
 
+        #Connection and retrival of each interface
         interface=None
         interface_output_list=[]
         for interface in interfaces_to_query:
@@ -67,6 +79,80 @@ def get_interface_info(device_details,interface_type, interface_number):
     return interface_output_list
 
 
+def regex_processor(interface_text, host):
+    """
+    Inputs:
+        <switch_data> reciebes the switch data of each interface
+    """
+    interface_info = {}
+
+    # Extract interface name
+    interface_name_match = re.search(r'^(\S+) is', interface_text, re.MULTILINE)
+    interface_info['interface_name'] = interface_name_match.group(1).lower() if interface_name_match else "NULL"
+
+    # Extract last input time
+    last_input_match = re.search(r'Last input (never|\d{2}:\d{2}:\d{2})', interface_text)
+    interface_info['last_input'] = last_input_match.group(1) if last_input_match else "NULL"
+
+    # Extract last output time
+    last_output_match = re.search(r'output (never|\d{2}:\d{2}:\d{2})', interface_text)
+    interface_info['last_output'] = last_output_match.group(1) if last_output_match else "NULL"
+
+    # Log time 
+    interface_info['log_time'] = datetime.now(ZoneInfo("Europe/Madrid")).strftime("%Y-%m-%d %H:%M:%S")
+
+    # Description of the port
+    description_match = re.search(r'Description: (.+)', interface_text)
+    interface_info['description'] = description_match.group(1).strip() if description_match else "NULL"
+
+    # Duplex type
+    duplex_match = re.search(r'(Full-duplex|Half-duplex|Auto-duplex)', interface_text)
+    interface_info['duplex_status'] = duplex_match.group(1) if duplex_match else "NULL"
+
+    # Speed of the interface
+    speed_match = re.search(r'(\d+Mb/s|\d+Gb/s|Auto-speed)', interface_text)
+    interface_info['speed'] = speed_match.group(1) if speed_match else "NULL"
+
+    # State of the interface
+    state_match = re.search(r'is (down|administratively down|up), line protocol is (down|up)', interface_text)
+    if state_match:
+        admin_state = state_match.group(1)
+        protocol_state = state_match.group(2)
+        if admin_state == "down":
+            interface_info['state'] = "Not connected"
+        elif admin_state == "administratively down":
+            interface_info['state'] = "Shutdown"
+        elif admin_state == "up" and protocol_state == "up":
+            interface_info['state'] = "Connected"
+        else:
+            interface_info['state'] = "Unknown"
+    else:
+        interface_info['state'] = "NULL"
+
+    #MAC interface of the connected device, set to NULL as no collection has been made yet
+    interface_info['mac'] = "NULL"
+    
+    """# Switchport mode of the interface
+    switchport_mode_match = re.search(r'switchport\s+mode\s+(access|trunk|dynamic\s+(auto|desirable))', interface_host_text, re.IGNORECASE)
+    if switchport_mode_match:
+        mode = switchport_mode_match.group(1).title()
+        interface_info['switchport_mode'] = mode
+        if mode == "Access":
+            vlan_match = re.search(r'switchport\s+access\s+vlan\s+(\d+)', interface_host_text, re.IGNORECASE)
+            interface_info['vlan'] = int(vlan_match.group(1)) if vlan_match else None
+        elif mode == "Trunk":
+            trunk_vlan_match = re.search(r'switchport\s+trunk\s+allowed\s+vlan\s+(.+)', interface_host_text, re.IGNORECASE)
+            interface_info['vlan'] = trunk_vlan_match.group(1) if trunk_vlan_match else "No VLAN Filtering"
+        else:
+            interface_info['vlan'] = "N/A"
+    else: # exception if there is no switchport configurations
+        interface_info['switchport_mode'] = None 
+        interface_info['vlan'] = None
+    """
+    interface_info['switch'] = host # Fixed IP of the switch (todo: implement reading of the current switch)
+    
+    return interface_info
+
 
 
 def orchestrator():
@@ -83,7 +169,7 @@ def orchestrator():
     json_switch_details=read_json_configs(json_switch_filepath,read_type)
 
 
-    switch_interface_detail=[]
+    all_switch_data=[]
     #Iterate list of Switches from the json
     for switch_detail in json_switch_details: # type: ignore
         #Get regular interface info
@@ -96,8 +182,13 @@ def orchestrator():
             regular_ints_results.append(i)
 
         #print(regular_ints_results) #List with all the switch interfaces info.
-        switch_interface_detail[switch_detail["host"]]=regular_ints_results
+        switch_data = {switch_detail["host"]: regular_ints_results}
+        all_switch_data.append(switch_data)
 
-    print(switch_interface_detail)
-    
+
+    for switch_dict in all_switch_data:
+        for hostname, interfaces in switch_dict.items():
+            for interface_info in interfaces:
+                processed_info = regex_processor(interface_info, hostname)
+                print(processed_info)
 orchestrator()
